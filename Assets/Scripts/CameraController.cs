977bf4f3f1228db57f40902a419706d5d45faaaf
Nguyễn Kiên <yusei5283@gmail.com>
2022-06-28 13:02:05 +0100

    Adding parallax for depth

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,16 +6,26 @@ public class CameraController : MonoBehaviour
 {
     public Transform target;
 
+    public Transform farBackground, middleBackground;
+
+    private float lastXPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        lastXPos = transform.position.x;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);
+
+        float amountToMoveX = transform.position.x - lastXPos;
+
+        farBackground.position += new Vector3(amountToMoveX, 0f, 0f);
+        middleBackground.position += new Vector3(amountToMoveX * .5f, 0f, 0f);
+
+        lastXPos = transform.position.x;
     }
 }
