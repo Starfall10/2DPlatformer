60ddb38816c140f254813ecb1b1db5ed27f06d1c
Nguyễn Kiên <yusei5283@gmail.com>
2022-07-02 00:03:32 +0100

    allow player to take damage

---
+++ Assets/Scripts/PlayerHealthController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthController : MonoBehaviour
+{
+    public static PlayerHealthController instance;
+
+    public int currentHealth, maxHealth;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DealDamage() 
+    {
+        currentHealth--;
+
+        if(currentHealth <= 0)
+        {
+            gameObject.SetActive(false); 
+        }
+    }
+}
