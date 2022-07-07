49134cfd9752c1145fdae0cbfcc88d91d92a9ff9
Nguyễn Kiên <yusei5283@gmail.com>
2022-07-07 23:50:59 +0100

    Added Respawn Mechanics

--- Assets/Scripts/PlayerHealthController.cs
+++ Assets/Scripts/PlayerHealthController.cs
@@ -52,7 +52,8 @@ public class PlayerHealthController : MonoBehaviour
             if(currentHealth <= 0)
             {
                 currentHealth = 0;
-                gameObject.SetActive(false); 
+                //gameObject.SetActive(false); 
+                LevelManager.instance.RespawnPlayer();
             }
             else
             {
